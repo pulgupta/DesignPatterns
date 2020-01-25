7a3062b2f34f4d8d200e09b168c843e39f987d59
Pulkit Gupta <pulgupta@gmail.com>
2020-01-25 20:57:59 +0530

    added more details to decorator

--- src/org/study/patterns/decorator/better/example/DecoratorTester.java
+++ src/org/study/patterns/decorator/better/example/DecoratorTester.java
@@ -8,20 +8,21 @@ package org.study.patterns.decorator.better.example;
  */
 public class DecoratorTester {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         BasicCar bc = new BasicCar();
         bc.setEngineCapacity(1200);
         bc.setWheelSize(15);
 
         LuxuryCar lc = new LuxuryCar(bc);
         lc.setLeatherMake("premium");
-        // The basic idea of using these decorators is that we can now mix and match multiple combinations 
-	// If you see below our sports car now have leather seats even though we do not have a single
-	// implementation of Car interface with all the features of luxury and sports car.
+
         SportsCar sc = new SportsCar(lc);
         sc.setDrivingModes(4);
         sc.setPeakTorqueMods(2);
 
+        // The basic idea of using these decorators is that we can now mix and match multiple combinations
+        // If you see below our sports car now have leather seats even though we do not have a single
+        // implementation of Car interface with all the features of luxury and sports car.
         Car sportsLuxuryCar = sc;
         sportsLuxuryCar.assemble();
     }
