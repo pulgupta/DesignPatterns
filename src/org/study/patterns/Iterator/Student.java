776373b9d277e698af09a6001148bc8d5acf67cc
Pulkit Gupta <pulgupta@gmail.com>
2020-01-26 21:22:14 +0530

    updated

--- src/org/study/patterns/Iterator/Student.java
+++ src/org/study/patterns/Iterator/Student.java
@@ -12,3 +12,4 @@ public final class Student {
         return name;
     }
 }
+
--- src/org/study/patterns/Iterator/StudentRecord.java
+++ src/org/study/patterns/Iterator/StudentRecord.java
@@ -1,6 +1,5 @@
 package org.study.patterns.Iterator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
