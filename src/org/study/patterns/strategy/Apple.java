7d9af59089796bd7ee7ee89df0e8b7c40c4202f5
Pulkit Gupta <pulgupta@gmail.com>
2020-01-27 22:01:49 +0530

    added notes for strategy pattern

--- src/org/study/patterns/strategy/Apple.java
+++ src/org/study/patterns/strategy/Apple.java
@@ -2,8 +2,10 @@ package org.study.patterns.strategy;
 
 /**
  * Context class
+ * “In computer programming, the strategy pattern (also known as the policy pattern) is a software design pattern
+ * that enables an algorithm's behavior to be selected at runtime. The strategy pattern. defines a family of
+ * algorithms, encapsulates each algorithm, and makes the algorithms interchangeable within that family.”
  * @author pulgupta
- *
  */
 public class Apple {
 
@@ -22,7 +24,10 @@ public class Apple {
 	public void setSize(String size) {
 		this.size = size;
 	}
-	
+
+	// This is the method which will be changed to changed the strategy.
+	// If you see closely we are passing this instance to the strategy.
+	// This ensures that all the strategies has the data which it require for execution.
 	public void printApple(PrettyPrintApple p) {
 		p.prettyPrintApple(this);
 	}
